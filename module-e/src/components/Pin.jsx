dec4d8b5caf227f2561dfb4b6b36d85e1d5bad98
JarnKet <ketsadaphoneOfficial@gmail.com>
2025-08-26 23:21:02 +0700

    Remove unused components, constants, and styles from module-e
    - Deleted UploadArea component and its related actions and context.
    - Removed CallToActionButton and ReadItLoudButton components.
    - Eliminated constants related to photo slider actions and themes.
    - Cleared out event data and footer content.
    - Removed sections related to contact, coupon game, essential information, event calendar, reviews, and video.
    - Deleted styles from index.css and Tailwind configuration.
    - Cleaned up Vite configuration and related assets.

--- module-e/src/components/Pin.jsx
+++
@@ -1,8 +0,0 @@
-export default function Pin({ children }) {
-  return (
-    <div className="w-[50px] h-[50px] bg-red-500 rounded-full relative flex justify-center items-center">
-      <p className="font-bold text-white text-center">{children}</p>
-      <div className="absolute bottom-0 left-1/2 translate-y-[79%] -translate-x-1/2 border-[20px] border-solid border-transparent border-t-red-500"></div>
-    </div>
-  );
-}
